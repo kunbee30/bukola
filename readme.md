296b68e244f345e771f27ab9a0c0b3d3159c5d6d
root <bawodein@gmail.com>
2022-04-14 21:13:53 +0000

    First trial

---
+++ readme.md
@@ -0,0 +1,5 @@
+This is a read me file
+
+This script in the repo is to create users and do some other task
+You can run this using "bash script.sh" or "./script.sh"
+Thank you
---
+++ script.sh
@@ -0,0 +1,11 @@
+#!/bin/bash
+
+echo "Welcome ti scripting lifestyle"
+sleep 4
+echo
+echo
+echo "If you like i can create a user with a folder for you"
+sleep 3
+echo
+echo
+echo "Would you like me to go ahead and create a personal user for you"
