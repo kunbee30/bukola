50f1e2a0d09d1bee5e1c4b9a07285987185cc644
kunbee30 <103294831+kunbee30@users.noreply.github.com>
2022-04-14 20:34:56 -0500

    New changes

--- script.sh
+++ script.sh
@@ -9,7 +9,7 @@ sleep 3
 echo
 echo
 echo "Would you like me to go ahead and create a personal user for you"
-read ans
+#read ans
 write an if statement if the ans is yes continue if the answer is no quit
 echo "Thanks for using my services"
 echo
